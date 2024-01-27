7ea015934469981ae6fa7daf03f2fd70d7649b2f
tfxTanoli <usman5194999@gmail.com>
2024-01-27 08:19:52 +0500

    Guessing A Number Game In Python

---
+++ GuessingGame.py
@@ -0,0 +1,14 @@
+secretNumber = 15
+
+print("You Have 3 Chances To Guess The Number ")
+print("Hint : Number Is Less Than 25...")
+
+i = 1
+while i <= 3:
+    guessedNumber = int(input("Enter Number : "))
+    if guessedNumber == secretNumber:
+        print("You Win...")
+        break
+    if i == 3:
+        print("You Failed...")
+    i += 1
